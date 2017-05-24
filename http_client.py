9f0e5b867ffc0883b6d067753e3319f5f66358f8
toylaa <tcmcd1337@gmail.com>
2017-05-24 13:12:14 -0400

    Add files via upload
    Initial Commit of finalized working versions of Client and Server programs : 
     - http_client.py
     - http_server.py

---
+++ http_client.py
@@ -0,0 +1,111 @@
+# @author Tyler McDonough 
+# @tcmcd1337@gmail.com
+## cps530_01
+### http_client.py
+
+# Import socket module
+from socket import *	
+
+#initialize loop control variable
+first = True			
+
+while True:
+	
+	#logic for first iteration
+	if first:
+		print "\n\n------------------------------------------------------------------------"
+		print "			[_TcMpYtHoN_Client_2.0_]"
+		print "------------------------------------------------------------------------"
+		serverName=raw_input(">Please enter Server Name/IP: ")
+		serverPort=int(raw_input(">Please enter Server Port #: "))
+		fileName = raw_input(">Which File to get from server: ")
+		fileToWrite = raw_input(">Local file to write data from server: ")
+		
+		# Command option control variable
+		# After first iteration provide user with options to re-use connection
+	
+	#logic for subsequent iterations
+	if first == False:
+		print """ 
+		\n\n------------------------------------------------------------------------
+		
+->> To Continue, please enter a new Server Name/IP...	
+	**OR**
+>Enter a client command: 
+	--> 'same' --- Reconnect to same server and port
+	--> 'end'  --- Shutdown Client	
+	--> 'kill' --- Shutdown Server"""		
+				
+		options=raw_input("-->:")		#Prompt for IP address/client options
+		
+		#shutdown client when 'end' is entered 
+		if options.lower() == 'end':
+			break
+		#shutdown SERVER remotely when 'kill' is entered
+		if options.lower() == 'kill':		
+			clientSocket = socket(AF_INET, SOCK_STREAM)
+			clientSocket.connect((serverName, serverPort))
+			clientSocket.send('kill')		
+			print " \n..The plumage don't enter into it. It's stone dead. "
+			break
+		
+		# retain IP and port info when 'same' is entered
+		if options.lower() == 'same':
+			print ">>Connection to Server @ ["+ serverName + ":" + str(serverPort) + "] reinstantiated"
+		
+		#only require additional port # when creating new connection 
+		else:
+			serverPort=int(raw_input(">Please enter Server Port #: "))
+		
+		fileName  = raw_input(">Which File to get from server: ")
+		fileToWrite=raw_input(">Local file to write data from server: ")
+
+	# Create a TCP server socket, connect it to the port # given
+		
+	clientSocket = socket(AF_INET, SOCK_STREAM)
+	clientSocket.connect((serverName, serverPort))
+	
+	# compose an http request header for a GET request
+	requestMessage = "GET /"+fileName + " HTTP/1.1 \r\n" + "User-agent: Python \r\nFrom: toylaa@aol.com"
+
+	clientSocket.send(requestMessage)		# Send the request through the socket	
+
+	# Receive the bytes from the socket
+	requestResult =clientSocket.recv(1024)	
+	print "\n>>> " + requestResult	
+	
+	serverReturn = clientSocket.recv(1024)	## receive actual file contents
+	
+	# if incoming data has length 0, print to console; break.
+	if len(serverReturn) == 0:
+		print "Server File Found :  '" + fileName + "'\n"
+		print "**Incoming data from file has length zer0..-->**BREAK**"
+		break
+			
+	# parse http header response - if NOT 404 -- file found logic
+	if requestResult.split()[1] <> '404':		
+		try:
+			# Open the local file to which incoming data (server's response)  is to be written. 
+			file = open( fileToWrite  , "w")
+			
+		except IOError:		#catch IOErrors
+			print("Could not open/Create file. No valid destination filename entered.")
+			break  
+			
+		# write to console.
+		print "\n>>Contents returned from server: '" + fileName +"'\n"+ serverReturn 	
+		print "\n\n>> Written to file --> ' " + fileToWrite	+ " '"
+		
+		file.write(str(serverReturn))	# write to file.		
+		file.close	# Close(/save) the opened local file.
+	
+	# parse http header response - if 404 -- file NOT found logic
+	elif int(requestResult.split()[1])==404: 
+		print "....Requested file '" + fileName + "' not found on server. "
+		print "		-Please try again."
+	
+	# loop option control - mark 'first' as false after first iteration
+	if first == True:
+		first = False		
+						
+	clientSocket.close 				# Close the socket connection
---
+++ http_server.py
@@ -0,0 +1,95 @@
+# @author Tyler McDonough 
+# @tcmcd1337@gmail.com
+## cps530_01
+### http_server.py
+
+
+# Import socket module
+from socket import *    
+
+# Create a TCP server socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+#Get from user a port number, and assign to the socket
+
+print "\n\n------------------------------------------------------------------------"
+print "			[_TcMpYtHoN_Server_2.0_]"
+print "------------------------------------------------------------------------"
+serverPort = raw_input("Please Enter a port number: ")
+serverPort = int(serverPort)
+
+# Bind the socket to server address and server port
+serverSocket.bind(("",serverPort))
+
+# Start listening to at most 1 connection at a time
+serverSocket.listen(1)
+
+# Server should be up and running and listening to the incoming connections
+while True:
+	print "\n>Ready to Serve... "		
+	# Set up a new connection from the client    
+	connectionSocket,addr = serverSocket.accept()
+	
+	# If an exception occurs during the execution of try block
+	# the rest of the block is skipped
+	# If the exception type matches the word after "except" statement
+	# the except clause is executed
+	try:
+		# Receives the request message from the client
+		message =  connectionSocket.recv(1024)
+
+                # You can print this message to see how an http request looks like.
+                # Request format is specified in the http RFC
+                # Write code to print incoming http request
+
+		print "\n>Recieved message from client: \n" + message + "\n"
+		
+		# Kill server logic
+		if message.lower()=='kill':
+			print "...\n..\n.\n\n'No, no, It's uh,...just resting."
+			connectionSocket.close()
+			break
+		
+		# Extract the path of the requested file from the message
+        # If you split the message into tokens, the filename will be the second token
+		# The path is the second part of HTTP header, identified by index [1]
+                                
+		filename = message.split()[1]
+		print "\n>Requested file name: " + filename		
+                
+		# get rid of the leading '/'
+		filename = filename[1:]		
+
+        # Store the entire content of the requested file in a temporary buffer				
+		file = open( filename  , "rb")
+		outputdata = file.read()
+		print "Contents: \n" + outputdata
+		print "------------------------------------------------------------------------"
+
+        # Send the HTTP response header line to the connection socket
+		connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n")
+ 
+		# Send the content of the requested file to the connection socket                
+		connectionSocket.send(outputdata)
+		print "\n\nSent: \n" + outputdata
+		
+		# Close the client connection socket                
+		connectionSocket.close()
+		print "------------------------------------------------------------------------"
+                
+	except IOError:
+		# Send HTTP response message for file not found
+		connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n")
+		connectionSocket.send("<html><head></head><body><h1>404 Not Found - Please Check your filename!</h1></body></html>\r\n")
+		print "\n>>File Not Found Error 404: Http Response sent:"
+		print "HTTP/1.1 404 Not Found\r\n\r\n" 
+		print "------------------------------------------------------------------------"
+		
+		# Close the client connection socket		
+        connectionSocket.close()
+		
+# close the server socket
+serverSocket.close()
+                
+
+
